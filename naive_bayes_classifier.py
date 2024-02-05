4d910105b0261e9db06b543086feaf8f6463e994
Luna Kilborn <leland.kilborn@hotmail.com>
2024-02-04 20:31:26 -0500

    Add evaluation to v2

--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -1,15 +1,16 @@
 import os
 import csv
+import random
 
 os.chdir('data')
 
-train_file = "subject lines.train"
-test_file = "subject lines.test"
+train_file_path = "subject lines.train"
+test_file_path = "subject lines.test"
 
 print("Spam Detector by L Kilborn")
 print()
-print(f"Training file: {train_file}")
-print(f"Testing file: {test_file}")
+print(f"Training file: {train_file_path}")
+print(f"Testing file: {test_file_path}")
 print()
 
 print("Tallying word usage ...", end='')
@@ -84,3 +85,88 @@ for word in words.keys():
     word_probs[word] = [spam_prob, ham_prob]
 
 print(" done")
+print()
+
+print("Evaluating performance ...", end='')
+
+false_positives = 0
+false_negatives = 0
+true_positives = 0
+true_negatives = 0
+
+spam_prob = spam_subjects / total_subjects
+ham_prob = 1 - spam_prob
+
+# calculate the confidence that a subject is spam
+def calc_spam_conf(words):
+    result = 1
+    for word in words:
+        if not (word in word_probs.keys()): 
+            continue
+        result *= word_probs[word][0]
+    return result * spam_prob
+
+# calculate the confidence that a subject is ham
+def calc_ham_conf(words):
+    result = 1
+    for word in words:
+        if not (word in word_probs.keys()):
+            continue
+        result *= word_probs[word][1]
+    return result * ham_prob
+
+# pick a choice
+def pick_classification(spam_conf, ham_conf):
+    if spam_conf > ham_conf:
+        return 1
+    elif ham_conf > spam_conf:
+        return 0
+    else: # if the probs are equal, then we make a random selection based on the overal probs
+        return int(random.random() < spam_prob)
+
+# predicts the result of a subject line
+def predict(subject: str):
+    words = get_words(subject)
+    spam_conf = calc_spam_conf(words)
+    ham_conf = calc_ham_conf(words)
+    return pick_classification(spam_conf, ham_conf)
+
+# adds up to a false/true pos/neg counters
+def tally(actual, guess):
+    global true_positives
+    global true_negatives
+    global false_negatives
+    global false_positives
+    if actual == 1 and guess == 1:
+        true_positives += 1
+    elif actual == 0 and guess == 0:
+        true_negatives += 1
+    elif actual == 1 and guess == 0:
+        false_negatives += 1
+    elif actual == 0 and guess == 1:
+        false_positives += 1
+
+test_file = open("subject lines.test", newline="\n", encoding="utf8")
+test_reader = csv.reader(test_file, delimiter="\t", quotechar="\"")
+
+output = open("predictions.csv", mode="w", newline="\n", encoding="utf8")
+output_writer = csv.writer(output, delimiter=",", quotechar="\"")
+output_writer.writerow(["spam", "pred", "subject"])
+
+for test in test_reader:
+    actual = int(test[0])
+    guess = predict(test[1])
+    tally(actual, guess)
+    output_writer.writerow([actual, guess, test[1]])
+
+output.close()
+
+print(" done")
+print(f"True Positives: {true_positives}")
+print(f"True Negatives: {true_negatives}")
+print(f"False Positives: {false_positives}")
+print(f"False Negatives: {false_negatives}")
+print()
+print(f"Precision: {float(true_positives) / float(true_positives + false_positives)}")
+print(f"Recall: {float(true_positives) / float(true_positives + false_negatives)}")
+print(f"F1: {(2 * true_positives) / (2 * true_positives + false_positives + false_negatives)}")
